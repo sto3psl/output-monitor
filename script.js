4c4f918e8c9f35a0d72ef1bb8f8b68169b0c17a6
sto3psl <stoepsl93@gmx.de>
2015-06-21 12:31:33 +0200

    lange strings werden besser dargestellt

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+// Twitter Feed
+var config1 = {
+  'id': '612537302854844416',
+  'domId': 'feed',
+  'maxTweets': 5,
+  'enableLinks': true
+}
+
+twitterFetcher.fetch(config1)
+
+// setInterval(function () {
+//   twitterFetcher.fetch(config1)
+//   console.log('updated')
+// }, 1000 * 10) // update every 10 seconds
+
 var day = '2015/06/21'
 
 var events = {
@@ -14,8 +29,30 @@ var events = {
     'people': 'Andreas Peetz',
     'type': 'Workshop',
     'room': 'E064'
+  },
+  '15:20:00': {
+    'name': 'Der Mehrwert von Informationsvisualisierungen',
+    'description': ' ',
+    'people': 'Bettina Kirchner, Martin Herrmann, Jan Wojdziak',
+    'type': 'Vortrag',
+    'room': 'E064'
+  },
+  '15:30:00': {
+    'name': 'Efficient Database Query Processing in Heterogeneous Environments',
+    'description': ' ',
+    'people': 'Tomas Karnagel',
+    'type': 'Vortrag',
+    'room': 'E064'
+  },
+  '15:40:00': {
+    'name': 'Mehr als die Summe seiner Teile:',
+    'description': 'Entwicklung selbst-adaptiver Anwendungen für Gemischte Interaktion',
+    'people': 'Maria Piechnick und Christian Piechnick',
+    'type': 'Vortrag',
+    'room': 'E064'
   }
 }
+
 var event = Object.keys(events)
 console.log(event[0])
 
@@ -34,7 +71,19 @@ var setEvent = function (event) {
     }
   }
 
-  document.querySelector('#name').innerHTML = events[event].name
+  var checkString = function (string) {
+    console.log('Length: ' + string.length)
+    if (string.length > 20 && string.length < 30) {
+      document.querySelector('h1').style.fontSize = '1.8em'
+      return string
+    } else if (string.length > 30) {
+      document.querySelector('h1').style.fontSize = '1.6em'
+      return string
+    }
+    return string
+  }
+
+  document.querySelector('#name').innerHTML = checkString(events[event].name)
   document.querySelector('#description').innerHTML = events[event].description
   document.querySelector('#people').innerHTML = events[event].people
   document.querySelector('#type').innerHTML = events[event].type
@@ -48,19 +97,4 @@ var setEvent = function (event) {
 //   setEvent(event[1])
 // }, 1000 * 5)
 
-setEvent(event[1])
-
-// Twitter Feed
-var config1 = {
-  'id': '612537302854844416',
-  'domId': 'feed',
-  'maxTweets': 5,
-  'enableLinks': true
-}
-
-twitterFetcher.fetch(config1)
-
-// setInterval(function () {
-//   twitterFetcher.fetch(config1)
-//   console.log('updated')
-// }, 1000 * 10) // update every 10 seconds
+setEvent(event[4])
